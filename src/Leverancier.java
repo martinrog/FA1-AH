2d6b94f9bd96776719dd1f49029491e6f12de17e
martinrog <martinrog14@gmail.com>
2021-07-27 14:20:07 +0200

    Attributes and setters made

--- src/Leverancier.java
+++ src/Leverancier.java
@@ -1,2 +1,18 @@
 public class Leverancier {
+
+    private String LeverancierNaam;
+    private String LeverancierAdres;
+    private String LeverancierTelefoonnummer;
+
+    public void setLeverancierNaam(java.lang.String leverancierNaam) {
+        LeverancierNaam = leverancierNaam;
+    }
+
+    public void setLeverancierAdres(java.lang.String leverancierAdres) {
+        LeverancierAdres = leverancierAdres;
+    }
+
+    public void setLeverancierTelefoonnummer(java.lang.String leverancierTelefoonnummer) {
+        LeverancierTelefoonnummer = leverancierTelefoonnummer;
+    }
 }
--- src/Werknemer.java
+++ src/Werknemer.java
@@ -1,2 +1,22 @@
 public class Werknemer {
+
+    private String werknemerNaam;
+    private String werknemerAdres;
+    private int werknemerTelefoonnummer;
+
+    public void setWerknemerNaam(java.lang.String werknemerNaam) {
+        this.werknemerNaam = werknemerNaam;
+    }
+
+    public void setWerknemerAdres(java.lang.String werknemerAdres) {
+        this.werknemerAdres = werknemerAdres;
+    }
+
+    public void setWerknemerTelefoonnummer(int werknemerTelefoonnummer) {
+        this.werknemerTelefoonnummer = werknemerTelefoonnummer;
+    }
+
+    public java.lang.String getWerknemerNaam() {
+        return werknemerNaam;
+    }
 }

9a09b82f497d6d7a94b0782b6c448ff06f67b211
martinrog <martinrog14@gmail.com>
2021-07-27 13:35:43 +0200

    First test setup of a new "Klant"

--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
 public class main {
+
+    public static void main (String[] args) {
+
+        Klant customer1 = new Klant();
+        customer1.setNaam("Martin");
+        customer1.setAdres("Hoornse Hop 9");
+        customer1.setOrderNummer(1234567890);
+        customer1.toString1();
+    }
 }

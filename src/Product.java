5698d6776a1ad1d565066c06feac54a1becafa4a
martinrog <martinrog14@gmail.com>
2021-07-27 13:47:43 +0200

    Attributes and setters made

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,18 @@
 public class Product {
+
+    private String productNaam;
+    private double productPrijs;
+    private int artikelNummer;
+
+    public void setProductNaam(java.lang.String productNaam) {
+        this.productNaam = productNaam;
+    }
+
+    public void setProductPrijs(double productPrijs) {
+        this.productPrijs = productPrijs;
+    }
+
+    public void setArtikelNummer(int artikelNummer) {
+        this.artikelNummer = artikelNummer;
+    }
 }

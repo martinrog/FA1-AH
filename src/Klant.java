2d801e6b21e785133046ddea1f64721a6c421bce
martinrog <martinrog14@gmail.com>
2021-07-27 13:34:56 +0200

    Attributes and setters made

--- src/Klant.java
+++ src/Klant.java
@@ -1,2 +1,25 @@
 public class Klant {
+
+    private String naam;
+    private int orderNummer;
+    private java.lang.String adres;
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setOrderNummer(int orderNummer) {
+        this.orderNummer = orderNummer;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+
+    public void toString1() {
+        System.out.println("Naam = " + naam);
+        System.out.println("Adres = " + adres);
+        System.out.println("Ordernummer = " + orderNummer + "\n");
+    }
 }

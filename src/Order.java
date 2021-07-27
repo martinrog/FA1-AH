04172cdb80b484a6f9c27b9646529cf6f2ba9ae1
martinrog <martinrog14@gmail.com>
2021-07-27 13:41:19 +0200

    Attributes and setters made

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,21 @@
 public class Order {
+
+    private int orderNummer;
+    private String productenLijst;
+    private double totaalPrijs;
+
+
+    public void setOrderNummer(int orderNummer) {
+        this.orderNummer = orderNummer;
+    }
+
+    public void setProductenLijst(String productenLijst) {
+        this.productenLijst = productenLijst;
+    }
+
+    public void toString2(){
+        System.out.println("ordernummer = "+ orderNummer);
+        System.out.println("productenlijst = "+ productenLijst);
+        System.out.println("totaalprijs = "+ totaalPrijs+ "\n");
+    }
 }

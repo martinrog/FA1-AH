c5b9ec2c4ebc31586ee4c24a2221f20f66d8ea09
martinrog <martinrog14@gmail.com>
2021-07-31 19:46:30 +0200

    Order updated

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Order {
 
+    private ArrayList<Product>  product_list = new ArrayList<Product>();
+
+    private ArrayList<Double> prices = new ArrayList<Double>();
+
     private int orderNummer;
+    private Product product;
     private String productenLijst;
     private double totaalPrijs;
 
@@ -9,8 +16,32 @@ public class Order {
         this.orderNummer = orderNummer;
     }
 
-    public void setProductenLijst(String productenLijst) {
-        this.productenLijst = productenLijst;
+    public void voeg_product_toe(Product product1) {
+
+        product_list.add(product1);
+    }
+
+
+
+
+
+
+    public double gettotaalprice(ArrayList<Product> product_list1){
+        ArrayList<Double> prices1 = prices;
+        for (Product i : product_list1){
+            if(i != null){
+                double singel_price = i.getProductPrijs();
+                prices1.add(singel_price);
+
+            }
+
+
+        }
+        return prices1.stream().mapToDouble(f -> f.doubleValue()).sum();
+
+    }
+    public double getTotaalprice() {
+        return totaalprice;
     }
 
     public void toString2(){
@@ -18,4 +49,5 @@ public class Order {
         System.out.println("productenlijst = "+ productenLijst);
         System.out.println("totaalprijs = "+ totaalPrijs+ "\n");
     }
+
 }

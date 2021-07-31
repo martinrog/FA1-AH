3a2b638f8becdb3a4574dd4a7b0088a51017d08d
martinrog <martinrog14@gmail.com>
2021-07-31 19:46:06 +0200

    pin or cash option + productlist

--- src/Klant.java
+++ src/Klant.java
@@ -3,9 +3,17 @@ public class Klant {
     private String naam;
     private int orderNummer;
     private java.lang.String adres;
+    private Order order;
+    private ArrayList<Product>  producten_lijst = new ArrayList<Product>();
+
+
 
     public void setNaam(String naam) {
         this.naam = naam;
+    }
+
+    public java.lang.String getNaam() {
+        return naam;
     }
 
     public void setOrderNummer(int orderNummer) {
@@ -16,10 +24,36 @@ public class Klant {
         this.adres = adres;
     }
 
+    public static Aankoop betaling_keuze(String klant_naam, String betaalmethode ) {
+        switch (betaalmethode) {
+            case "pin":
 
-    public void toString1() {
-        System.out.println("Naam = " + naam);
-        System.out.println("Adres = " + adres);
-        System.out.println("Ordernummer = " + orderNummer + "\n");
+                return new betaling_pin(klant_naam);
+            case "cash":
+                return new betaling_cash(klant_naam); }
+
+
+
+        throw new IllegalArgumentException();
+
+
+
+    }
+
+    public String order(Aankoop method) {
+        return method.betalen(order.gettotaalprice(producten_lijst));
+
+    }
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Klant{" +
+                "naam=" + naam +
+                ", orderNummer=" + orderNummer +
+                ", adres='" + adres + '\'' +
+                ", order=" + order +
+                ", producten_lijst=" + producten_lijst +
+                '}';
     }
 }
